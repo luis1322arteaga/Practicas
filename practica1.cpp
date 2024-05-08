2c9df396b97dbeec63da2100e182e6a821dcde42
luis1322arteaga <163915242+luis1322arteaga@users.noreply.github.com>
2024-05-08 12:53:01 -0600

    Add files via upload

---
+++ practica1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int i = 1, m = 12, r = 0;
+
+    while (i <= 12)
+    {
+        r = i * m;
+        cout << r << "\n";
+        i++;
+    }
+    return 0;
+}
